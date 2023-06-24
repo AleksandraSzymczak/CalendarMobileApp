d60cc3d8fdee9d4ea0385721cb4bdebd09e1016d
AleksandraSzymczak <aleksis550@wp.pl>
2023-06-24 15:08:10 +0200

    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# CalendarMobileApp
- Aplikacja mobilna "Kalendarz" to narzędzie, które umożliwia użytkownikom organizację i planowanie różnych wydarzeń, spotkań i aktywności.
---
+++ app/src/main/java/com/example/calendarmobileapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.calendarmobileapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.calendarmobileapp.databinding.MainCalendarViewBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: MainCalendarViewBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = MainCalendarViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.pickDate.setOnClickListener{ViewNotes()}
+    }
+    fun ViewNotes() {
+        setContentView(R.layout.activity_main)
+    }
+}
