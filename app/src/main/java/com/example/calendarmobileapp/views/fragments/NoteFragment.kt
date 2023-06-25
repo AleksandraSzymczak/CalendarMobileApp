95f929178cb352053ea803b61be9989d73cfc124
AleksandraSzymczak <aleksis550@wp.pl>
2023-06-26 00:24:39 +0200

    dodany accelerometer

--- app/src/main/java/com/example/calendarmobileapp/views/fragments/NoteFragment.kt
+++ app/src/main/java/com/example/calendarmobileapp/views/fragments/NoteFragment.kt
@@ -1,5 +1,10 @@
 package com.example.calendarmobileapp.views.fragments
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -15,11 +20,16 @@ import com.example.calendarmobileapp.databinding.FragmentNoteBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.lang.System.currentTimeMillis
 
-class NoteFragment : Fragment() {
+class NoteFragment : Fragment(), SensorEventListener {
     private val mainVm by activityViewModels<MainViewModel>()
     private var _binding:FragmentNoteBinding? = null
     private val binding get() = _binding!!
+    private lateinit var sensorManager: SensorManager
+    private var accelerometer: Sensor? = null
+    private  var lastShakeTime: Long = 0
+    private val cooldownDuration: Long = 1000
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,6 +40,9 @@ class NoteFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        sensorManager = requireActivity().getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
         mainVm.getNote()?.let { bindNoteData(it) }
         binding.DeleteButton.setOnClickListener {
             mainVm.getNote()?.let { note ->
@@ -53,9 +66,33 @@ class NoteFragment : Fragment() {
         super.onResume()
         view?.invalidate()
     }
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+            val x = event.values[0]
+            val acceleration = Math.sqrt((x * x).toDouble())
+            val currentTime = System.currentTimeMillis()
+            if(acceleration > 8 &&
+                currentTime - lastShakeTime >= cooldownDuration) {
+                lastShakeTime = currentTime
+                mainVm.getNote()?.let { note ->
+                    lifecycleScope.launch {
+                        withContext(Dispatchers.IO) {
+                            mainVm.deleteNote(note)
+                            mainVm.loadDataFromDBSpecificDate()
+                        }
+                        Toast.makeText(requireContext(), "Usunięto notatkę", Toast.LENGTH_SHORT)
+                            .show()
+                        findNavController().popBackStack()
+                    }
+                }
+            }
+        }
+    }
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        sensorManager.unregisterListener(this)
     }
 
 }
